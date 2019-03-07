b2b1a2130ef69a42a8474181c3c8a59f0734f147
Henrique Couto <henrique.coutu@live.com>
2019-03-07 18:13:51 -0300

    alguns métodos de get

--- src/app.py
+++ src/app.py
@@ -1,9 +1,40 @@
-from bottle import run, route
+from bottle import run, route, get
+
+users = [
+    {
+        'name': 'Henrique Couto',
+        'user': 'henrique',
+        'password': '123',
+        'type': 'admin'
+    },
+    {
+        'name': 'Rychard Souza',
+        'user': 'rychard',
+        'password': '123',
+        'type': 'admin'
+    },
+    {
+        'name': 'Fulaninho de Tal',
+        'user': 'fulaninho',
+        'password': '123',
+        'type': 'client'
+    }
+]
 
 
-@route('/')
-def hello():
-    return 'Hello, world!!!!!'
+@get('/users')
+def getAll():
+    return {'users': users}
 
 
-run(host='localhost', port=8080)
+@get('/users/<name>')
+def getUser(name):
+    finded_user = {}
+    for user in users:
+        if user['user'] == name:
+            finded_user = user
+    return {'user': finded_user}
+
+
+if __name__ == '__main__':
+    run(reloader=True, port=8080, host='localhost')
