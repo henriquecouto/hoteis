133a8adee5297816ad10ac6b8014a9f9de8e29d5
Rychard souza <rychard34@outlook.com>
2019-03-08 20:07:38 -0300

    iniciando persistencia no mongo

--- src/app.py
+++ src/app.py
@@ -1,72 +1,29 @@
-from bottle import run, route, get, post, request, delete
+from bottle import Bottle
+import json
+from bottle.ext.mongo import MongoPlugin
 
-users = [
-    {
-        'name': 'Henrique Couto',
-        'user': 'henrique',
-        'password': '123',
-        'type': 'admin'
-    },
-    {
-        'name': 'Rychard Souza',
-        'user': 'rychard',
-        'password': '123',
-        'type': 'admin'
-    },
-    {
-        'name': 'Fulaninho de Tal',
-        'user': 'fulaninho',
-        'password': '123',
-        'type': 'client'
-    }
-]
+app = Bottle()
+database_name = 'heroku_4q7l2460'
+db_uri = 'mongodb://hoteis:oioi12@ds137336.mlab.com:37336/heroku_4q7l2460'
+db_plugin = MongoPlugin(uri=db_uri,db=database_name)
 
+app.install(db_plugin)
 
-@get('/users')
-def getAllUsers():
-    return {'users': users}
+@app.route('/users')
+def getAll(mongodb):
+    query = mongodb['users'].find()
+    results = []
+    for q in query:
+        results.append(q['user'])
+    return {'result':results}
 
-
-@get('/users/<name>')
-def getUser(name):
-    finded_user = {}
-    for user in users:
-        if user['user'] == name:
-            finded_user = user
-    return {'user': finded_user}
-
-
-@post('/users')
-def addUser():
-    new_user = request.json
-    users.append(new_user)
-    return {'newUser': new_user}
-
-
-@post('/users/<name>')
-def changeUser(name):
-    finded_user = {}
-    for user in users:
-        if user['user'] == name:
-            finded_user = user
-    if(finded_user):
-        new_user = request.json
-        users.remove(finded_user)
-        users.append(new_user)
-        return {'userChanged': new_user}
-    else:
-        return {'userChanged': 'Not finded'}
-
-
-@delete('/users/<name>')
-def deleteUser(name):
-    finded_user = {}
-    for user in users:
-        if user['user'] == name:
-            finded_user = user
-    users.remove(finded_user)
-    return {'removedUser': finded_user}
-
+@app.route('/users/<name>')
+def getOne(mongodb,name):
+    query = mongodb['users'].find({'user':name})
+    result = []
+    for q in query:
+        result.append(q['user'])
+    return {'result':result}
 
 if __name__ == '__main__':
-    run(reloader=True, port=8080, host='localhost')
+    app.run(debug=True,reloader=True, port=8080,host='localhost')
