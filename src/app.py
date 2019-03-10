0d9d3d8de2cd0548bd0c04859f3e3a86e61581e4
Henrique Couto <henrique.coutu@live.com>
2019-03-09 21:00:30 -0300

    Início do CRUD de reservas

--- src/app.py
+++ src/app.py
@@ -1,29 +1,40 @@
-from bottle import Bottle
-import json
+from bottle import Bottle, request, response
 from bottle.ext.mongo import MongoPlugin
+from bson.json_util import dumps
+import json
 
 app = Bottle()
 database_name = 'heroku_4q7l2460'
 db_uri = 'mongodb://hoteis:oioi12@ds137336.mlab.com:37336/heroku_4q7l2460'
-db_plugin = MongoPlugin(uri=db_uri,db=database_name)
+db_plugin = MongoPlugin(uri=db_uri, db=database_name, json_mongo=True)
 
 app.install(db_plugin)
 
-@app.route('/users')
-def getAll(mongodb):
-    query = mongodb['users'].find()
-    results = []
-    for q in query:
-        results.append(q['user'])
-    return {'result':results}
 
-@app.route('/users/<name>')
-def getOne(mongodb,name):
-    query = mongodb['users'].find({'user':name})
-    result = []
-    for q in query:
-        result.append(q['user'])
-    return {'result':result}
+@app.route('/reservas', method='POST')
+def createReserva(mongodb):
+    newReserva = request.json
+
+    query = mongodb['reservas'].find({'quarto': newReserva['quarto']})
+
+    reservas = dumps(query)
+
+    disponivel= True
+    status= 'Reserva Disponível'
+
+    if(newReserva['saida'] < newReserva['entrada']):
+        return {'result': 'A data de entrada deve ser antes da data de saída'}
+    else:
+        for reserva in json.loads(reservas):
+            if (reserva['saida'] >= newReserva['entrada']) and (reserva['entrada'] <= newReserva['saida']) :
+                disponivel = False
+                status = 'Reserva não disponível'
+
+    if(disponivel):
+        # criar reserva
+        return {'result': status}
+    else:
+        return {'result': status}
 
 if __name__ == '__main__':
-    app.run(debug=True,reloader=True, port=8080,host='localhost')
+    app.run(debug=True, reloader=True, port=8080, host='localhost')
